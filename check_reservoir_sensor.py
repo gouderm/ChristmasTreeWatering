5e0fd0eed7d1b062211e65fdfffbe24099b8e8e5
gouderm <70639593+gouderm@users.noreply.github.com>
2023-12-23 17:12:32 +0100

    Add check for both reservoir and tree sensor

---
+++ check_reservoir_sensor.py
@@ -0,0 +1,27 @@
+from main import check_water_level, init_gpio
+import constants as c
+import RPi.GPIO as GPIO
+import time
+
+if __name__ == "__main__":
+    try:
+        GPIO.setmode(GPIO.BCM)
+
+        init_gpio(
+                c.GPIO_tree_sensor,
+                c.GPIO_tree_ref, 
+                c.GPIO_reservoir_sensor,
+                c.GPIO_reservoir_ref,
+                c.GPIO_alarm,
+                c.GPIO_pump
+            )
+
+        while True:
+            if check_water_level(c.GPIO_reservoir_sensor, c.GPIO_reservoir_ref):
+                GPIO.output(c.GPIO_alarm, GPIO.LOW)
+            else:
+                GPIO.output(c.GPIO_alarm, GPIO.HIGH)
+            time.sleep(0.1)
+
+    finally:
+        GPIO.cleanup()
